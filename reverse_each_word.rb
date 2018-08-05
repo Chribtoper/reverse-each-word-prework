f30af9dd87099a0cf83b6a4b47f126cdeaad6043
Chribtoper <xcstolfax@gmail.com>
2018-08-05 23:16:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(sentence)
 array = sentence.split
 finalarray = []
+newarray = []
 array.each do |string|
-  newarray = 
+  newarray.push(string)
+  newarray.split(//)
 end
 
     
