0ae0168a5c8040867abf31da1ef6556a1d346b65
Chribtoper <xcstolfax@gmail.com>
2018-08-05 23:18:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,11 @@ newarray = []
 array.each do |string|
   newarray.push(string)
   newarray.split(//)
+  newarray.reverse
+  newstring = newarray.join
+  finalarray.push(newstring)
+  newarray = []
 end
-
-    
+final = finalarray.join
+end
   
